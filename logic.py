e333ab66b2224e80e6e88f8980377361ebf5126f
muy <maryanne.uy2007@gmail.com>
2026-05-02 22:38:03 -0500

    changes

--- logic.py
+++ logic.py
@@ -8,7 +8,38 @@ class Logic(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
         self.button_power.clicked.connect(lambda : self.power())
+        self.slider_volume.sliderMoved.connect(lambda : self.volume())
+        self.slider_channel.sliderMoved.connect(lambda: self.channel())
+        self.radioButton.clicked.connect(lambda : self.mute())
+
+        self.button_0.clicked.connect(lambda : self.channel_0())
+        self.button_1.clicked.connect(lambda: self.channel_1())
+        self.button_2.clicked.connect(lambda: self.channel_2())
+        self.button_3.clicked.connect(lambda: self.channel_3())
+        self.button_4.clicked.connect(lambda: self.channel_4())
+        self.button_5.clicked.connect(lambda: self.channel_5())
 
     def power(self):
-        screen = self.screen()
-        screen.
+        pass
+
+    def volume(self):
+        pass
+
+    def channel(self):
+        pass
+
+    def mute(self):
+        pass
+
+    def channel_0(self):
+        pass
+    def channel_1(self):
+        pass
+    def channel_2(self):
+        pass
+    def channel_3(self):
+        pass
+    def channel_4(self):
+        pass
+    def channel_5(self):
+        pass
