dc273dffd56643ed8b912d1e2a6aecc7db0741fe
muy <maryanne.uy2007@gmail.com>
2026-05-02 22:18:09 -0500

    made mvc

---
+++ logic.py
@@ -0,0 +1,8 @@
+from PyQt6.QtWidgets import *
+from gui import *
+
+
+class Logic(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(Logic, self).__init__()
+        self.setupUi(self)
