e22ebb76d4214ef13f5784b86b838df7a1f313b5
muy <maryanne.uy2007@gmail.com>
2026-05-02 22:21:01 -0500

    changes

--- logic.py
+++ logic.py
@@ -6,3 +6,8 @@ class Logic(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(Logic, self).__init__()
         self.setupUi(self)
+
+        self.button_power.clicked.connect(lambda : self.power())
+
+    def power(self):
+        
