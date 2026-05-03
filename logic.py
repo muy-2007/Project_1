0d4544d7186e4e0743401b56c5f28df13f7703ce
muy <maryanne.uy2007@gmail.com>
2026-05-02 23:12:10 -0500

    changes

--- logic.py
+++ logic.py
@@ -4,6 +4,11 @@ from gui import *
 
 class Logic(QMainWindow, Ui_MainWindow):
     def __init__(self):
+        '''
+        Used to create the main window
+        param: self
+        return: None
+        '''
         super(Logic, self).__init__()
         self.setupUi(self)
 
